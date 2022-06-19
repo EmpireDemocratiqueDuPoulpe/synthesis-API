03f31b7883f1b334bff62a6c7ba8f73bb618affb
EmpireDemocratiqueDuPoulpe <alexis.lecomte@supinfo.com>
2022-06-19 12:35:01 +0200

    Add authenticator

--- api/routes/backups.js
+++ api/routes/backups.js
@@ -20,60 +20,54 @@ export default (router) => {
 	/* ---- CREATE ---------------------------------- */
 	/* ---- READ ------------------------------------ */
 	/**
-	 * GET /v1/absences/all
-	 * @summary Get all absences
+	 * GET /v1/backups/download-latest
+	 * @summary Download the latest backup file
 	 * @security BearerAuth
-	 * @tags Absences
+	 * @tags Backups
 	 *
 	 * @param {string} brokilone.header.required - Auth header
-	 * @param {string} years.query - Years
-	 * @param {string} userIDs.query - User IDs
-	 * @param {string} campusIDs.query - Campus IDs
 	 *
-	 * @return {SuccessResp} 200 - **Success**: the absences are returned - application/json
-	 *
-	 * @example response - 200 - Success response
-	 * { "code": 200, "absences": [
-	 *  { "absence_id": 1, "start_date": "2022-03-20T23:00:00.000Z", "end_date": "2022-03-21T23:00:00.000Z", "user_id": 2 }
-	 * ]}
+	 * @return {SuccessResp} 200 - **Success**: the backup file is sent - application/json}
 	 */
-	route.get("/download-latest", async (request, response, next) => {
-		// Get the path to the backups directory
-		let fullPath = API.backups.path;
+	route.get("/download-latest", authenticator, async (request, response, next) => {
+		if (await request.user.hasAllPermissions("EXPORT_DATA")) {
+			// Get the path to the backups directory
+			let fullPath = API.backups.path;
 
-		if (!fullPath) {
-			throw new APIError(500, "Le répertoire de sauvegarde est introuvable.");
-		}
+			if (!fullPath) {
+				throw new APIError(500, "Le répertoire de sauvegarde est introuvable.");
+			}
 
-		if (!path.isAbsolute(fullPath)) {
-			const currentFile = DirName(import.meta.url);
-			fullPath = path.join(currentFile.__dirname, fullPath);
-		}
+			if (!path.isAbsolute(fullPath)) {
+				const currentFile = DirName(import.meta.url);
+				fullPath = path.join(currentFile.__dirname, fullPath);
+			}
 
-		if (!fs.existsSync(fullPath)) {
-			throw new APIError(500, "Le répertoire de sauvegarde est introuvable.");
-		}
+			if (!fs.existsSync(fullPath)) {
+				throw new APIError(500, "Le répertoire de sauvegarde est introuvable.");
+			}
 
-		// Get all files in this folder
-		const files = (await fsAsync.readdir(fullPath))
-			.map(file => {
-				const filePath = path.join(fullPath, file);
-				const stats = fs.lstatSync(filePath);
+			// Get all files in this folder
+			const files = (await fsAsync.readdir(fullPath))
+				.map(file => {
+					const filePath = path.join(fullPath, file);
+					const stats = fs.lstatSync(filePath);
 
-				return { path: filePath, isFile: stats.isFile(), lastModified: stats.mtime.getTime() };
-			})
-			.filter(file => file.isFile)
-			.sort((fileA, fileB) => fileB.lastModified - fileA.lastModified);
+					return { path: filePath, isFile: stats.isFile(), lastModified: stats.mtime.getTime() };
+				})
+				.filter(file => file.isFile)
+				.sort((fileA, fileB) => fileB.lastModified - fileA.lastModified);
 
-		if (!files.length) {
-			throw new APIError(500, "Aucune sauvegarde n'a été trouvée.");
-		}
+			if (!files.length) {
+				throw new APIError(500, "Aucune sauvegarde n'a été trouvée.");
+			}
 
-		// Send the last file
-		const lastFile = files[0];
+			// Send the last file
+			const lastFile = files[0];
 
-		response.download(lastFile.path);
-		logger.log("Download the last backup file", { ip: request.clientIP, params: {code: 200, file: lastFile.path} });
+			response.download(lastFile.path);
+			logger.log("Download the last backup file", { ip: request.clientIP, params: {code: 200, file: lastFile.path} });
+		} else next(new APIError(403, "Permission denied: couldn't access this endpoint."));
 	});
 
 	/* ---- UPDATE ---------------------------------- */
